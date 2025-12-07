f5f336ed3e8e51a5e4c7c281d106dd13dfa4973b
chuxiangshi2026 <chuxiangshi2026@qq.com>
2025-12-07 14:31:34 +0800

    添加划词添加到列表功能

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,125 @@
 
 
 <script setup lang="ts">
-import {RouterView} from 'vue-router'
+import {RouterView, useRouter} from 'vue-router'
 
-import {onMounted} from 'vue';
+import {onMounted, onUnmounted, ref} from 'vue';
 import {useWordsStore} from "@/stores/words.ts";
 // import {storeToRefs} from "pinia";
 import {DEFAULT_INTERVALS} from "@/constants";
 import {addWord} from "@/utils/str-util.ts";
+import {ElMessage} from "element-plus";
 
 const wordsStore = useWordsStore();
 // const {words} = storeToRefs(wordsStore)
+
+// 添加模拟按键方法
+const simulateKeyOperations = {
+  // 模拟复制操作
+  copy: () => {
+    window.services.simulateCopy();
+  }
+}
+
+
+window.utools.onPluginEnter(async (action: PluginEnterAction) => {
+
+
+  /*  // app版本
+    const currentVerson = window.services.getAppVerson()
+    // 数据库版本
+    const previousVerson = window.services.wordModel.getAppVersionFromDb()
+    // 有返回false   null返回ture
+    let b = !previousVerson?.version;
+    if (
+        //   ?.  链式调用，空返回 undef
+        b ||
+        currentVerson !== previousVerson?.version
+    ) {
+      // 没有版本或版本不一致   指定为最新版本
+      window.services.wordModel.setAppVerson(currentVerson)
+      // 显示更新通知
+      // dispatch(updateshowNotification(true))
+      console.log('新版，更新version', currentVerson)
+    }*/
+
+
+  // await initUtoolSetting()
+
+  console.log("action对象", JSON.stringify(action))
+  if (action.code === 'over') {
+    // 把单词翻译了，添加到 列表中
+    // console.log('==================', action)
+
+    await handlePluginAddWord(action)
+  }
+
+  if (action.code === 'huaci' && action.type == 'text') {
+
+
+    getSelectedTextFromSystem().then(text => {
+
+          // 5. 判断逻辑（根据你的场景调整阈值）
+          const textError = (
+              text.length <= 0 ||
+              text.endsWith('\n') ||          // 以换行符结尾（整行复制的典型特征）
+              text.length > 25                // 长度超过合理选中范围
+          );
+          if (textError) {
+            ElMessage.error('请先用光标选中单词');
+          } else {
+            addWord(text)
+            // ElMessage.success(text);
+          }
+        }
+    );
+
+  }
+
+  if (action.code === 'review') {
+    handlePluginReview()
+  }
+
+
+})
+
+
+// ==================== 核心：静默获取选中文本 ====================
+async function getSelectedTextFromSystem(): Promise<string> {
+
+
+  utools.copyText('')
+
+  utools.hideMainWindow();
+
+  await new Promise(r => setTimeout(r, 50)); // 等待焦点恢复
+
+  // 获取当前平台
+  const isMac = utools.isMacOS();
+
+// 根据平台选择修饰键
+  const modifier = isMac ? "command" : "ctrl";
+  utools.simulateKeyboardTap("c", modifier);
+
+  utools.showMainWindow();
+
+  // console.log('ok',ok)
+  // 3. 等待系统完成复制（100ms 足够）
+  // 2. 使用可靠的延迟方式（推荐 200-300ms）
+  await new Promise(resolve => setTimeout(resolve, 50));
+
+  // 4. 读取剪贴板
+  const selectedText = await navigator.clipboard.readText();
+  // console.log('新内容', selectedText);
+
+
+  // 5. 恢复原始剪贴板内容
+  // if (originalClipboard) {
+  //  await  navigator.clipboard.writeText(originalClipboard);
+  // }
+
+  return selectedText;
+}
 
 
 onMounted(() => {
@@ -21,47 +130,13 @@ onMounted(() => {
   // 首页刷新时触发   自动更新需要复习的单词
   updateReview();
 
+
+  // window.addEventListener('selected-text', handleSelectedText as EventListener);
+
+
   // 进入插件
 
-  window.utools.onPluginEnter(async (action: PluginEnterAction) => {
 
-
-
-    /*  // app版本
-      const currentVerson = window.services.getAppVerson()
-      // 数据库版本
-      const previousVerson = window.services.wordModel.getAppVersionFromDb()
-      // 有返回false   null返回ture
-      let b = !previousVerson?.version;
-      if (
-          //   ?.  链式调用，空返回 undef
-          b ||
-          currentVerson !== previousVerson?.version
-      ) {
-        // 没有版本或版本不一致   指定为最新版本
-        window.services.wordModel.setAppVerson(currentVerson)
-        // 显示更新通知
-        // dispatch(updateshowNotification(true))
-        console.log('新版，更新version', currentVerson)
-      }*/
-
-
-    // await initUtoolSetting()
-
-
-    if (action.code === 'over') {
-      // 把单词翻译了，添加到 列表中
-      // console.log('==================', action)
-
-      await handlePluginAddWord(action)
-    }
-
-    if (action.code === 'review') {
-      handlePluginReview()
-    }
-
-
-  })
   /*  const initUtoolSetting = () => {
       return new Promise((resolve) => {
         let setting = window.services.wordModel.getUtoolsSetting()
