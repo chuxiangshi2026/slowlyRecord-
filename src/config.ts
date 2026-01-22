47cb3b2214132a456cebbae451380fcbec3ec1ad
chuxiangshi2026 <chuxiangshi2026@qq.com>
2026-01-22 17:31:02 +0800

    移动说明文件图片到子目录

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,17 @@
 export const AppInfo = {
-  // appkey: 'YOUR_OLD_YOUDAO_APPKEY2',
-  appkey: 'YOUR_OLD_YOUDAO_APPKEY',
-  key: 'YOUR_OLD_YOUDAO_KEY'
-  // key: 'YOUR_OLD_YOUDAO_KEY2'
+    // appkey: 'YOUR_OLD_YOUDAO_APPKEY2',
+    "youdao": {
+        appkey: 'YOUR_OLD_YOUDAO_APPKEY',
+        key: 'YOUR_OLD_YOUDAO_KEY'
+    },
+    "ali": {
+        appkey: 'YOUR_ALI_APPKEY',
+        key: 'YOUR_ALI_KEY'
+    },
+    "baidu": {
+        // appid
+        appkey: 'YOUR_BAIDU_APPKEY',
+        // key: 'YOUR_BAIDU_KEY_OLD2'
+        key: 'YOUR_BAIDU_KEY'
+    }
 }
